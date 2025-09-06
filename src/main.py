9ad8c629daeb651d9fe72563e085a7d76b4e1ba4
Kissaki2Pointer <61546552+Kissaki2Pointer@users.noreply.github.com>
2025-09-07 00:18:04 +0900

    [変更]曜日を取得し表示する。

--- src/main.py
+++ src/main.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 
 from logger import slog
+from datetime import datetime
 
 def main():
     slog("START", "TAQIKOを起動します。")
+    
+    weekdays = ["月", "火", "水", "木", "金", "土", "日"]
+    current_weekday = weekdays[datetime.now().weekday()]
+    
+    slog("INFO", f"今日は{current_weekday}曜日です。")
+
+    slog("END", "TAQIKOを終了します。")
 
 if __name__ == "__main__":
     main()
