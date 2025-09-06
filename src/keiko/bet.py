bff192cf46de34b2fe091061db49888c8929e94f
Kissaki2Pointer <61546552+Kissaki2Pointer@users.noreply.github.com>
2025-09-07 00:43:41 +0900

    [変更] 週末なら競馬投票自動化ツール、平日なら株自動売買ツールを実行する。

---
+++ src/keiko/bet.py
@@ -0,0 +1,4 @@
+from logger import slog
+
+def web_login(): 
+	slog("INFO", f"WEBログインを実行します。")
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 from logger import slog
 from datetime import datetime
+from keiko.bet import web_login
 
 def main():
     slog("START", "TAQIKOを起動します。")
@@ -10,6 +11,12 @@ def main():
     current_weekday = weekdays[datetime.now().weekday()]
     
     slog("INFO", f"今日は{current_weekday}曜日です。")
+    
+    if current_weekday in ["土", "日"]:
+        slog("INFO", "競馬自動投票ツールを実行します。")
+        web_login()
+    else:
+        slog("INFO", "株自動売買ツールを実行します")
 
     slog("END", "TAQIKOを終了します。")
 
