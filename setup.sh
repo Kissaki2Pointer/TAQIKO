e8d31d8ad93e8a1003f2055da9d734ef8f61fbaa
Kissaki2Pointer <61546552+Kissaki2Pointer@users.noreply.github.com>
2025-09-09 20:59:52 +0900

    [変更] setup.shを修正

--- setup.sh
+++ setup.sh
@@ -16,13 +16,13 @@ sudo apt-get install -y build-essential wget
 
 # ta-libのダウンロードとインストール
 echo "ta-libをダウンロード中..."
-wget http://prdownloads.sourceforge.net/ta-lib/ta-lib-0.6.4-src.tar.gz
+wget https://github.com/ta-lib/ta-lib/releases/download/v0.6.4/ta-lib-0.6.4-src.tar.gz
 
 echo "ta-libを展開中..."
 tar -xzvf ta-lib-0.6.4-src.tar.gz
 
 echo "ta-libをコンパイル・インストール中..."
-cd ta-lib
+cd ta-lib-0.6.4
 ./configure --prefix=/usr
 make
 sudo make install
@@ -35,10 +35,11 @@ sudo ldconfig
 
 # クリーンアップ
 echo "一時ファイルをクリーンアップ中..."
-rm -rf ta-lib-0.6.4-src.tar.gz ta-lib
+rm -rf ta-lib-0.6.4-src.tar.gz ta-lib-0.6.4
 
 # Pythonライブラリのインストール
 echo "Pythonライブラリをインストール中..."
+python -m pip install TA-Lib
 
 echo "========================================="
 echo "セットアップが完了しました。"
